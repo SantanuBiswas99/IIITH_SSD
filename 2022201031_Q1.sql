b8411cfe0c9101761c9ec68b44634ac8f4aa7aea
Santanu Biswas <biswassantanu22@gmail.com>
2022-08-18 17:10:49 +0530

    Delete 2022201031_Q1.sql

--- 2022201031_Q1.sql
+++
@@ -1,4 +0,0 @@
-select distinct concat(Fname, Minit, Lname) as "Full name", Ssn, Dnumber, Dname from (select Mgr_ssn, Dname, Dnumber from DEPARTMENT where Dnumber in
-(select Dno from EMPLOYEE where Ssn in 
-(select  D.Essn from (select Essn, Pno, IFNULL(Hours, 0) as Hours from WORKS_ON) as D group by D.Essn having sum(D.Hours) < 40.0))) as Depart
-join EMPLOYEE as emp where emp.Ssn = Depart.Mgr_ssn;
